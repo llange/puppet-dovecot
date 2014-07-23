f0c6e4ebd3ed3e01f302564632e37b1378a8dac9
nicolasb827 <nicolas.belan@gmail.com>
2014-07-23 12:36:59 +0200

    fix names and sql requests

--- manifests/init.pp
+++ manifests/init.pp
@@ -196,7 +196,7 @@ class dovecot (
     file { '/etc/dovecot/conf.d/20-lmtp.conf':
         content => template('dovecot/conf.d/20-lmtp.conf.erb'),
     }
-    file { '/etc/dovecot/conf.d/20-managesieve.conf.conf':
+    file { '/etc/dovecot/conf.d/20-managesieve.conf':
         content => template('dovecot/conf.d/20-managesieve.conf.erb'),
     }
     file { '/etc/dovecot/conf.d/90-sieve.conf':
